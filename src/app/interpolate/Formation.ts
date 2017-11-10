8ffbe35e051ef46baf2759c197c47a2314a12d0c
Thibault Goudouneix <nilmanduil@gmail.com>
2017-11-10 11:46:41 +0100

    Many changes

--- src/app/interpolate/Formation.ts
+++ src/app/interpolate/Formation.ts
@@ -1,9 +1,15 @@
 export default class Formation {
   nom: string;
   description: string;
+  prix: number;
+  dateDebut: string;
+  dateFin: string;
 
-  constructor({nom, description}) {
+  constructor({nom, description, prix = 0, dateDebut = "", dateFin = ""}) {
     this.nom = nom;
     this.description = description;
+    this.prix = prix;
+    this.dateDebut = (dateDebut ? dateDebut : "01/01/1970");
+    this.dateFin = (dateFin ? dateFin : "01/01/1970");
   }
 }
--- src/app/interpolate/interpolate.component.ts
+++ src/app/interpolate/interpolate.component.ts
@@ -11,19 +11,61 @@ export class InterpolateComponent implements OnInit {
 
   prenom = 'Test prénom';
   nom = 'Test nom';
+  nameColor = "red";
   formation: Formation = new Formation({nom: "Angular", description: 'Formation pour DTA Ingénierie'});
   formationJS: Formation;
+  textHidden = true;
+  buttonLabel = "Afficher";
+  formations: Array<Formation> = [];
 
   constructor() { }
 
   getFullname(): string {
     return this.prenom + ' ' + this.nom;
   }
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.formationJS= new Formation({nom: 'Module JavaScript', description: "Formation très utile"});
-    }, 3000);
+  changeNameColor(): void {
+    this.nameColor = (this.nameColor === "red" ? "green" : "red");
   }
 
+  ngOnInit() {
+    setInterval( () => {
+      this.changeNameColor();
+      }, 500);
+    setTimeout(() => {
+      this.formationJS = new Formation({nom: 'Module JavaScript', description: "Formation très utile"});
+    }, 3000);
+
+    setTimeout(() => {this.formations.push(new Formation({nom: "Angular", description: 'Formation pour DTA Ingénierie', prix: 500, dateDebut: "01/01/2017", dateFin: "01/03/2017"}));}, 1000);
+    setTimeout(() => {this.formations.push(new Formation({nom: "JS", description: 'Formation pour DTA Ingénierie', prix: 1500, dateDebut: "01/04/2017", dateFin: "01/15/2017"}));}, 2000);
+    setTimeout(() => {this.formations.push(new Formation({nom: "Cordova", description: 'Formation pour DTA Ingénierie', prix: 700, dateDebut: "01/15/2017", dateFin: "01/20/2017"}));}, 3000);
+    setTimeout(() => {this.formations.push(new Formation({nom: "Swift", description: 'Formation pour DTA Ingénierie', prix: 3000, dateDebut: "02/01/2017", dateFin: "02/25/2017"}));}, 4000);
+    setTimeout(() => {this.formations.push(new Formation({nom: "Android", description: 'Formation pour DTA Ingénierie', prix: 500, dateDebut: "03/01/2017", dateFin: "03/03/2017"}));}, 5000);
+  }
+
+  toggleMyText(): void {
+    this.textHidden = !this.textHidden;
+    this.buttonLabel = (this.textHidden ? "Afficher" : "Cacher");
+  }
+
+  mouseOutOfText(): void {
+    console.log('Fin survol');
+  }
+
+  mouseOnText(): void {
+    console.log('Début survol');
+  }
+
+  getColorByElement(first, last): string {
+    if (first) {
+      return "red";
+    } else if (last) {
+      return "green";
+    } else {
+      return "black";
+    }
+  }
+
+  getBackgroundByElement(odd): string {
+    return (odd ? "grey" : "white");
+  }
 }
